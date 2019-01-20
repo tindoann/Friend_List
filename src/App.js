d9728c54a3cd23ffdb272a5c6431e75a8a503ebd
TinX <TinX@TinX.local>
2019-01-20 16:42:53 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import './App.css';
+
+const App = () => <PersonList />;
+
+const PersonList = () => {
+  const people = [
+    {
+      img: 22,
+      name: "john",
+      job: "developer"
+    },
+    {
+      img: 34,
+      name: "bob",
+      job: "designer"
+    },
+    {
+      img: 56,
+      name: "peter",
+      job: "artist"
+    }
+  ];
+
+  return (
+    <section>
+      <Person person={people[0]} />;
+      <Person person={people[1]}> Nostrud adipisicing magna anim magna incididunt et in ea nulla pariatur aliquip.
+      </Person>
+      ;
+      <Person person={people[2]} />;
+    </section>
+  );
+};
+
+const Person = props => {
+  const { img, name, job } = props.person;
+  const { children } = props; 
+  const url = `https://randomuser.me/api/portraits/thumb/men/${img}.jpg`;
+  
+    return (
+      <div className="person">
+        <img src={url} alt="" />
+        <div>
+          <h4>{name}</h4>
+          <h4>{job}</h4>
+          {children}
+        </div>
+      </div>
+  );
+};
+
+
+export default App;
